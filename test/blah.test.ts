12089360b972ac89d2959b9a0b7ede4813b0e30d
Fabricio Flores <fabrifloresg@gmail.com>
2023-11-12 09:17:12 -0500

    No ticket: agregar test (#2)

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,17 @@
-import { sum } from '../src';
+import { sum, substract } from '../src';
 
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
+  });
+});
+
+describe('substract', () => {
+  it('should substract 2 numbers', () => {
+    expect(substract(2, 1)).toEqual(1);
+  });
+
+  it('should substract 2 numbers even if result is negative', () => {
+    expect(substract(2, 5)).toEqual(-3);
   });
 });
